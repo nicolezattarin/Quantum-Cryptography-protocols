bcfa64789df20d8e26544e038da2f22576158f39
nicolezattarin <72666452+nicolezattarin@users.noreply.github.com>
2021-12-05 15:31:15 +0100

    change structure plot

--- QRNG/security.py
+++ QRNG/security.py
@@ -6,7 +6,7 @@ def security_parameter(H, errorH, length):
     errepsilon = 2**(length/2.-2-H/2.)*errorH*np.log(2)
     return epsilon, errepsilon
 
-def security(Hmin, Herr, length):
+def security(Hmin, Herr, length, path, patherr):
     """
     Using the min-entropies estimated in 
     the previous steps, study how the security parameter from 
@@ -41,14 +41,32 @@ def security(Hmin, Herr, length):
         errsecurity[str(h)] = err_eps
     security['length'] = length
     errsecurity['length'] = length
-    security.to_csv('security/security_untrusted.csv')
-    errsecurity.to_csv('security/errsecurity_untrusted.csv')
+    security.to_csv(path)
+    errsecurity.to_csv(patherr)
 
 def main():
-    Hmin = [0.975, 0.946, 8e-6, 0.580, 0.79]
-    Herr = [0.003, 0.005, 6e-6, 0.007, 0.01]
     length = np.arange(1, 10, 1)
-    security(Hmin, Herr, length)
+
+    #trusted
+    path ='security/security_trusted.csv'
+    patherr ='security/security_err_trusted.csv'
+    Hmin = [0.975, 0.946]
+    Herr = [0.003, 0.005]
+    security(Hmin, Herr, length, path, patherr)
+
+    #uncertainty
+    path ='security/security_uncert.csv'
+    patherr ='security/security_err_uncert.csv'
+    Hmin = [8e-6, 0.581, 0.798]
+    Herr = [6e-6, 0.004, 0.003]
+    security(Hmin, Herr, length, path, patherr)
+
+    #tomography
+    path ='security/security_tomo.csv'
+    patherr ='security/security_err_tomo.csv'
+    Hmin = [8e-6, 0.580, 0.79]
+    Herr = [6e-6, 0.007, 0.01]
+    security(Hmin, Herr, length, path, patherr)
 
 if __name__ == '__main__':
     main()
