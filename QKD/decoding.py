fa607675ec68b6d361207130cd3d104f2ab85789
nicolezattarin <72666452+nicolezattarin@users.noreply.github.com>
2022-01-03 18:22:59 +0100

    update and change data storage

--- QKD/decoding.py
+++ QKD/decoding.py
@@ -4,7 +4,7 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--filename", default='QKD_Lab_data/input-keys.decoy', help="filename.", type=str)
+parser.add_argument("--filename", default='QKD_Lab_data/input-keys.bob', help="filename.", type=str)
 parser.add_argument("--maxiter", default=None, help="maximum keys to read.", type=int)
 
 def main(filename, maxiter):
@@ -43,47 +43,73 @@ def main(filename, maxiter):
         block_sizes.append(block_size)
     
     #decode keys
+    decoded_states = []
     decoded_keys = []
+    decoded_basis = []
     count = 0
     for k in keys:
         print("Decoding key: ", count, " of ", len(keys))
-        decoded = []
+        states = []
+        basis = []
+        bits = []
         k = np.split(k, len(k)//2)
         if filename[-5:] == 'alice' or filename[-3:] == 'bob':
             for pair in k:
-                if (pair==[0,0]).all(): decoded.append('H')
-                elif (pair==[0,1]).all(): decoded.append('V')
-                elif (pair==[1,0]).all(): decoded.append('D')
-                elif (pair==[1,1]).all(): decoded.append('A')
-            decoded_keys.append(decoded)
+                if (pair==[0,0]).all(): 
+                    states.append('H')
+                    bits.append('0')
+                    basis.append('0')
+                elif (pair==[0,1]).all(): 
+                    states.append('V')
+                    bits.append('1')
+                    basis.append('0')
+                elif (pair==[1,0]).all(): 
+                    states.append('D')
+                    bits.append('0')
+                    basis.append('1')
+                elif (pair==[1,1]).all(): 
+                    states.append('A')
+                    bits.append('1')
+                    basis.append('1')
+            decoded_keys.append(''.join(bits))
+            decoded_states.append(''.join(states))
+            decoded_basis.append(''.join(basis))
             if maxiter != None and count == maxiter-1: break
             count+=1
         else:
             for pair in k:
-                if (pair==[0,0]).all(): decoded.append('S')
-                elif (pair==[0,1]).all(): decoded.append('L')
-                elif (pair==[1,0]).all(): decoded.append('Unused')
-                elif (pair==[1,1]).all(): decoded.append('Unused')
-            decoded_keys.append(decoded)
+                if (pair==[0,0]).all(): 
+                    states.append('S')
+                elif (pair==[0,1]).all(): 
+                    states.append('L')
+                elif (pair==[1,0]).all(): 
+                    states.append('Unused')
+                elif (states==[1,1]).all(): 
+                    states.append('Unused')
+            decoded_states.append(''.join(states))
             if maxiter != None and count == maxiter-1: break
             count+=1
-
-    decoded_keys = np.array(decoded_keys, dtype=object)
-    block_sizes = np.array(block_sizes, dtype=object)
-
+    
+    if filename[-5:] == 'alice' or filename[-3:] == 'bob':
+        decoded_keys = np.array(decoded_keys, dtype=object)
+        decoded_basis = np.array(decoded_basis, dtype=object)
+    block_sizes = np.array(block_sizes, dtype=int)
+    decoded_states = np.array(decoded_states, dtype=object)
     print(decoded_keys.shape, block_sizes.shape)
 
     #check if keys are correct
-    print ("Decoded key (first 3 bytes): ", decoded_keys[0][:12])
+    print ("Decoded key (first 3 bytes): ", decoded_states[0][:12])
 
     #save to csv
     import os
     dir = 'QKD_keys'
     if os.path.isdir(dir)==False:
         os.mkdir(dir)
-
-    df = pd.DataFrame(decoded_keys, columns=['key'])
+    df = pd.DataFrame(decoded_states, columns=['states'])
     df['block_size'] = block_sizes[:maxiter]
+    if filename[-5:] == 'alice' or filename[-3:] == 'bob':
+        df['key'] = decoded_keys
+        df['basis'] = decoded_basis
     if filename[-5:] == 'alice':
         df.to_csv(dir+'/decoded_keys_alice.csv', index=False)
     elif filename[-3:] == 'bob':
---
+++ QKD/parameters.py
@@ -0,0 +1,44 @@
+import warnings
+
+class parameters():
+    def __init__(self, alice_key_basis_prob, alice_check_basis_prob, 
+                    bob_key_basis_prob, bob_check_basis_prob,
+                    decoy_strong_prob, decoy_weak_prob,
+                    decoy_strong_intensity, decoy_weak_intensity,
+                    secrecy=1e-9, correctness = 1e-15):
+
+        if alice_check_basis_prob + alice_key_basis_prob != 1:
+            raise ValueError('Aice basis probability must sum to 1')
+                                
+        if alice_check_basis_prob > 1 or alice_check_basis_prob < 0 or \
+            alice_key_basis_prob > 1 or alice_key_basis_prob < 0 :
+            raise ValueError('Aice basis probability must be between 0 and 1')
+        
+        if bob_check_basis_prob + bob_key_basis_prob != 1:
+            raise ValueError('Bob basis probability must sum to 1')
+        
+        if bob_check_basis_prob > 1 or bob_check_basis_prob < 0 or \
+            bob_key_basis_prob > 1 or bob_key_basis_prob < 0 :
+            raise ValueError('Bob basis probability must be between 0 and 1')
+        
+        if decoy_strong_prob + decoy_weak_prob != 1:
+            raise ValueError('Decoy probability must sum to 1')
+        
+        if decoy_strong_prob > 1 or decoy_strong_prob < 0 or \
+            decoy_weak_prob > 1 or decoy_weak_prob < 0 :
+            raise ValueError('Decoy probability must be between 0 and 1')
+        
+        if decoy_strong_intensity < decoy_weak_intensity:
+            warnings.warn('Decoy strong intensity must be greater than decoy weak intensity')
+            decoy_strong_intensity, decoy_weak_intensity = decoy_weak_intensity, decoy_strong_intensity
+        
+        self.alice_key_basis_prob = alice_key_basis_prob
+        self.alice_check_basis_prob = alice_check_basis_prob
+        self.bob_key_basis_prob = bob_key_basis_prob
+        self.bob_check_basis_prob = bob_check_basis_prob
+        self.decoy_strong_prob = decoy_strong_prob
+        self.decoy_weak_prob = decoy_weak_prob
+        self.decoy_strong_intensity = decoy_strong_intensity
+        self.decoy_weak_intensity = decoy_weak_intensity
+        self.secrecy = secrecy
+        self.correctness = correctness
