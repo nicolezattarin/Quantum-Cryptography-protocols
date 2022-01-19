550795a33bfae49e2d00170abbbaa7dab742e571
nicolezattarin <72666452+nicolezattarin@users.noreply.github.com>
2022-01-19 17:17:42 +0100

    Update security_bounds.py

--- QKD/security_bounds.py
+++ QKD/security_bounds.py
@@ -4,7 +4,9 @@ import argparse
 from parameters import parameters
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--block_size", default=1500, help="block_size ",type=int)
+parser.add_argument("--block_size", default=int(1e6), help="block_size ",type=int)
+parser.add_argument("--frac_data", default=1, help="fracrtion of data to read ",type=float)
+parser.add_argument("--repetition_rate", default=1, help="repetition_rate of the soure ",type=float)
 parser.add_argument("--alice_key_basis_prob", default=0.9, help="alice key basis probability ",type=float)
 parser.add_argument("--alice_check_basis_prob", default=0.1, help="alice check basis probability ",type=float)
 parser.add_argument("--bob_key_basis_prob", default=0.5, help="bob key basis probability ", type=float)
@@ -15,7 +17,7 @@ parser.add_argument("--decoy_strong_intensity", default=0.4699, help="decoy weak
 parser.add_argument("--decoy_weak_intensity", default=0.1093, help="decoy weak pulse intensity ", type=float)
 
 
-def main(block_size,
+def main(block_size,frac_data, repetition_rate,
         alice_key_basis_prob, alice_check_basis_prob, 
         bob_key_basis_prob, bob_check_basis_prob,
         decoy_strong_prob, decoy_weak_prob,
@@ -27,8 +29,8 @@ def main(block_size,
                     decoy_strong_intensity, decoy_weak_intensity,
                     secrecy=1e-9, correctness=1e-15,
                     lambda_EC=1.16)
-    
-    df = pd.read_csv('results/countings_{}_{}frac.csv'.format(block_size, frac))
+
+    df = pd.read_csv('results/countings_{}_{}frac.csv'.format(block_size, frac_data))
     df=df.drop(df.index[-1], axis=0)
 
     # errors on countings and finite key effect
@@ -57,51 +59,50 @@ def main(block_size,
 
     df['n_key_mumax_plus'] = strong_prefactor*(df['n_key_mumax'] + deltaN_key)
     df['n_key_mumax_min'] = strong_prefactor*(df['n_key_mumax'] - deltaN_key)
-    df['n_key_mumax_err'] = strong_prefactor*np.sqrt(df['n_key_mumax_err']**2 \
-                                    +(np.log(19./p.secrecy)/(8*df['n_key_tot']))*df['n_key_tot_err']**2)
+    df['n_key_mumax_err_Hoeff'] = strong_prefactor*np.sqrt(df['n_key_mumax_err']**2 \
+                                    + (np.log(19./p.secrecy)/(8*df['n_key_tot']))*df['n_key_tot_err']**2)
+
 
     df['n_key_mumin_plus'] = weak_prefactor*(df['n_key_mumin'] + deltaN_key)
     df['n_key_mumin_min'] = weak_prefactor*(df['n_key_mumin'] - deltaN_key)
-    df['n_key_mumin_err'] = weak_prefactor*np.sqrt(df['n_key_mumin_err']**2 \
-                                    +(np.log(19./p.secrecy)/(8*df['n_key_tot']))*df['n_key_tot_err']**2)
+    df['n_key_mumin_err_Hoeff'] = weak_prefactor*np.sqrt(df['n_key_mumin_err']**2 \
+                                    + (np.log(19./p.secrecy)/(8*df['n_key_tot']))*df['n_key_tot_err']**2)
 
     df['n_check_mumax_plus'] = strong_prefactor*(df['n_check_mumax'] + deltaN_check)
     df['n_check_mumax_min'] = strong_prefactor*(df['n_check_mumax'] - deltaN_check)
-    df['n_check_mumax_err'] = strong_prefactor*np.sqrt(df['n_check_mumax_err']**2 \
-                                    +(np.log(19./p.secrecy)/(8*df['n_check_tot']))*df['n_check_tot_err']**2)
+    df['n_check_mumax_err_Hoeff'] = strong_prefactor*np.sqrt(df['n_check_mumax_err']**2 \
+                                    + (np.log(19./p.secrecy)/(8*df['n_check_tot']))*df['n_check_tot_err']**2)
 
     df['n_check_mumin_plus'] = weak_prefactor*(df['n_check_mumin'] + deltaN_check)
     df['n_check_mumin_min'] = weak_prefactor*(df['n_check_mumin'] - deltaN_check)
-    df['n_check_mumin_err'] = weak_prefactor*np.sqrt(df['n_check_mumin_err']**2 \
-                                    +(np.log(19./p.secrecy)/(8*df['n_check_tot']))*df['n_check_tot_err']**2)
+    df['n_check_mumin_err_Hoeff'] = weak_prefactor*np.sqrt(df['n_check_mumin_err']**2 \
+                                    + (np.log(19./p.secrecy)/(8*df['n_check_tot']))*df['n_check_tot_err']**2)
 
     df['m_key_mumax_plus'] = strong_prefactor*(df['m_key_mumax'] + deltaM_key)
     df['m_key_mumax_min'] = strong_prefactor*(df['m_key_mumax'] - deltaM_key)
-    df['m_key_mumax_err'] = strong_prefactor*np.sqrt(df['m_key_mumax_err']**2 \
-                                    +(np.log(19./p.secrecy)/(8*df['m_key_tot']))*df['m_key_tot_err']**2)
+    df['m_key_mumax_err_Hoeff'] = strong_prefactor*np.sqrt(df['m_key_mumax_err']**2 \
+                                    + (np.log(19./p.secrecy)/(8*df['m_key_tot']))*df['m_key_tot_err']**2)
 
     df['m_key_mumin_plus'] = weak_prefactor*(df['m_key_mumin'] + deltaM_key)
     df['m_key_mumin_min'] = weak_prefactor*(df['m_key_mumin'] - deltaM_key)
-    df['m_key_mumin_err'] = strong_prefactor*np.sqrt(df['m_key_mumin_err']**2 \
-                                    +(np.log(19./p.secrecy)/(8*df['m_key_tot']))*df['m_key_tot_err']**2)
+    df['m_key_mumin_err_Hoeff'] = strong_prefactor*np.sqrt(df['m_key_mumin_err']**2 \
+                                    + (np.log(19./p.secrecy)/(8*df['m_key_tot']))*df['m_key_tot_err']**2)
 
     df['m_check_mumax_plus'] = strong_prefactor*(df['m_check_mumax'] + deltaM_check)
     df['m_check_mumax_min'] = strong_prefactor*(df['m_check_mumax'] - deltaM_check)
-    df['m_check_mumax_err'] = weak_prefactor*np.sqrt(df['m_check_mumax_err']**2 \
-                                    +(np.log(19./p.secrecy)/(8*df['m_check_tot']))*df['m_check_tot_err']**2)
+    df['m_check_mumax_err_Hoeff'] = weak_prefactor*np.sqrt(df['m_check_mumax_err']**2 \
+                                    + (np.log(19./p.secrecy)/(8*df['m_check_tot']))*df['m_check_tot_err']**2)
 
     df['m_check_mumin_plus'] = weak_prefactor*(df['m_check_mumin'] + deltaM_check)
     df['m_check_mumin_min'] = weak_prefactor*(df['m_check_mumin'] - deltaM_check)
-    df['m_check_mumin_err'] = weak_prefactor*np.sqrt(df['m_check_mumin_err']**2 \
-                                    +(np.log(19./p.secrecy)/(8*df['m_check_tot']))*df['m_check_tot_err']**2)
-
-    # df=df[df.select_dtypes(include=[np.number]).ge(0).all(1)]
-    # df.index=range(len(df))
+    df['m_check_mumin_err_Hoeff'] = weak_prefactor*np.sqrt(df['m_check_mumin_err']**2 \
+                                    + (np.log(19./p.secrecy)/(8*df['m_check_tot']))*df['m_check_tot_err']**2)
 
     #qber
     df['QBER_key'] = df['m_key_tot']/df['n_key_tot']
     df['QBER_key_err'] = np.sqrt(df['m_key_tot_err']**2/df['n_key_tot']**2 + \
                                 df['m_key_tot']**2*df['n_key_tot_err']**2/df['n_key_tot']**4)
+
     df['QBER_check'] = df['m_check_tot']/df['n_check_tot']
     df['QBER_check_err'] = np.sqrt(df['m_check_tot_err']**2/df['n_check_tot']**2 +\
                                 df['m_check_tot']**2*df['n_check_tot_err']**2/df['n_check_tot']**4)
@@ -110,40 +111,30 @@ def main(block_size,
     df['s_key_0_low'] = (p.tau0/(p.d_strong_intensity-p.d_weak_intensity))*(p.d_strong_intensity*\
                         df['n_key_mumin_min'] - p.d_weak_intensity*df['n_key_mumax_plus'])
     df['s_key_0_low_err'] = (p.tau0/(p.d_strong_intensity-p.d_weak_intensity))* \
-                            np.sqrt(p.d_strong_intensity**2*(df['n_key_mumin_err']**2 + \
-                                    p.d_weak_intensity**2 * df['n_key_mumax_err']**2))
+                            np.sqrt(p.d_strong_intensity**2*(df['n_key_mumin_err_Hoeff']**2 + \
+                                    p.d_weak_intensity**2 * df['n_key_mumax_err_Hoeff']**2))
             
     # upper bound of zero photon detections
-    df['s_key_0_up_strong'] = 2*(p.tau0*(np.exp(p.d_strong_intensity)/p.d_strong_prob)*\
-                                df['m_key_mumax_plus']+np.sqrt(0.5*df['n_key_tot']*np.log(19/p.secrecy)))
-    df['s_key_0_up_strong_err'] = 2*((p.tau0*(np.exp(p.d_strong_intensity)/p.d_strong_prob))**2* df['m_key_mumax_err']**2 \
-                                    + (np.log(19/p.secrecy)/(8*df['n_key_tot']))*df['n_key_tot_err']**2)
-    df['s_key_0_up_weak'] = 2*(p.tau0*(np.exp(p.d_weak_intensity)/p.d_weak_prob)\
-                                *df['m_key_mumin_plus']+np.sqrt(0.5*df['n_key_tot']*np.log(19/p.secrecy)))
-    df['s_key_0_up_weak_err'] = 2*((p.tau0*(np.exp(p.d_weak_intensity)/p.d_weak_prob))**2* df['m_key_mumax_err']**2 \
-                                    + (np.log(19/p.secrecy)/(8*df['n_key_tot']))*df['n_key_tot_err']**2)
-
+    df['s_key_0_up_strong'] = 2*(p.tau0*df['m_key_mumax_plus']\
+                            + np.sqrt(0.5*df['n_key_tot']*np.log(19/p.secrecy)))
+    df['s_key_0_up_strong_err'] = 2*np.sqrt((p.tau0*df['m_key_mumax_err_Hoeff'])**2 \
+                            + (np.log(19/p.secrecy)/(8*df['n_key_tot']))*df['n_key_tot_err']**2)
 
     # lower bound of one photon detections key
     df['s_key_1_low'] = (p.tau1*p.d_strong_intensity/(p.d_weak_intensity*(p.d_strong_intensity-p.d_weak_intensity)))\
                         * (df['n_key_mumin_min'] \
-                        - (p.d_weak_intensity/p.d_strong_intensity)**2 *df['n_key_mumax_plus']\
-                        - (p.d_strong_intensity**2-p.d_weak_intensity**2)/(p.d_strong_intensity**2)*(df['s_key_0_up_weak']/p.tau0))
+                        - (p.d_weak_intensity/p.d_strong_intensity)**2*df['n_key_mumax_plus']\
+                        - (p.d_strong_intensity**2-p.d_weak_intensity**2)/(p.d_strong_intensity**2*p.tau0)*df['s_key_0_up_strong'])
                         
     df['s_key_1_low_err'] = (p.tau1*p.d_strong_intensity/(p.d_weak_intensity*(p.d_strong_intensity-p.d_weak_intensity)))\
-                            * np.sqrt((df['n_key_mumin_err']**2 + \
-                            + (p.d_weak_intensity/p.d_strong_intensity)**4 *df['n_key_mumax_err']**2\
-                            + ((p.d_strong_intensity**2-p.d_weak_intensity**2)/(p.d_strong_intensity**2))**2*(df['s_key_0_up_weak_err']/p.tau0)**2))
-
-
-
+                            * np.sqrt((df['n_key_mumin_err_Hoeff']**2 + \
+                            + (p.d_weak_intensity/p.d_strong_intensity)**4*df['n_key_mumax_err_Hoeff']**2\
+                            + ((p.d_strong_intensity**2-p.d_weak_intensity**2)/(p.d_strong_intensity**2))**2*(df['s_key_0_up_strong_err']/p.tau0)**2))
 
     # lower bound of one photon detections check everything needed fot phase error
-    df['s_check_0_up_weak'] = 2*(p.tau0*(np.exp(p.d_weak_intensity)/p.d_weak_prob)\
-                                *df['m_check_mumin_plus']+np.sqrt(0.5*df['n_check_tot']*np.log(19/p.secrecy)))
-    df['s_check_0_up_weak_err'] = 2*((p.tau0*(np.exp(p.d_weak_intensity)/p.d_weak_prob))**2* df['m_check_mumax_err']**2 \
-                                    + (np.log(19/p.secrecy)/(8*df['n_check_tot']))*df['n_check_tot_err']**2)
-
+    df['s_check_0_up_weak'] = 2*(p.tau0*df['m_check_mumin_plus']+np.sqrt(0.5*df['n_check_tot']*np.log(19/p.secrecy)))
+    df['s_check_0_up_weak_err'] = 2*np.sqrt((p.tau0*df['m_check_mumax_err_Hoeff'])**2 \
+                                + (np.log(19/p.secrecy)/(8*df['n_check_tot']))*df['n_check_tot_err']**2)
 
     df['s_check_1_low'] = (p.tau1*p.d_strong_intensity/(p.d_weak_intensity*(p.d_strong_intensity-p.d_weak_intensity)))\
                         * (df['n_check_mumin_min'] \
@@ -151,10 +142,54 @@ def main(block_size,
                         - (p.d_strong_intensity**2-p.d_weak_intensity**2)/(p.d_strong_intensity**2)*(df['s_check_0_up_weak']/p.tau0))
                         
     df['s_check_1_low_err'] = (p.tau1*p.d_strong_intensity/(p.d_weak_intensity*(p.d_strong_intensity-p.d_weak_intensity)))\
-                            * np.sqrt((df['n_check_mumin_err']**2 + \
-                            + (p.d_weak_intensity/p.d_strong_intensity)**4 *df['n_check_mumax_err']**2\
-                            + ((p.d_strong_intensity**2-p.d_weak_intensity**2)/(p.d_strong_intensity**2))**2*(df['s_check_0_up_weak_err']/p.tau0)**2))
+                            * np.sqrt((df['n_check_mumin_err_Hoeff']**2 + \
+                            + (p.d_weak_intensity/p.d_strong_intensity)**4 *df['n_check_mumax_err_Hoeff']**2\
+                            + ((p.d_strong_intensity**2-p.d_weak_intensity**2)/(p.d_strong_intensity**2*p.tau0))**2*df['s_check_0_up_weak_err']**2))
 
     df['v_check_1_up'] = (p.tau1/(p.d_strong_intensity-p.d_weak_intensity))*(df['m_check_mumax_plus'] - df['m_check_mumin_min'])
-    df['v_check_1_up_err'] = (p.tau1/(p.d_strong_intensity-p.d_weak_intensity))*np.sqrt(df['m_check_mumax_err']**2 + df['m_check_mumin_err']**2)
+    df['v_check_1_up_err'] = (p.tau1/(p.d_strong_intensity-p.d_weak_intensity))*np.sqrt(df['m_check_mumax_err_Hoeff']**2 + df['m_check_mumin_err_Hoeff']**2)
 
+    def gamma(a,b,c,d, eb, ec, ed): #not considering error on a
+            log = np.log(((c+d)/(c*d*b*(1-b)))*(21/a)**2)
+            gamma_p = np.sqrt(((c+d)*(1-b)*b)/(c*d)*log)
+            gamma = gamma_p/np.log(2)
+            partial_b = ((c+d)*(2*b-1)*(log-1))/(2*c*d*np.log(2)*gamma_p)
+            partial_c = (b*(b-1)*(log+1))/(2*c*c*np.log(2)*gamma_p)
+            partial_d = (b*(b-1)*(log+1))/(2*d*d*np.log(2)*gamma_p)
+            error = np.sqrt(eb**2*partial_b**2 + ec**2*partial_c**2 + ed**2*partial_d**2)
+            return gamma, error
+
+    err_temp = np.sqrt((df['v_check_1_up_err']/df['s_check_1_low'])**2\
+                        +(df['v_check_1_up']*df['s_check_1_low_err']/df['s_check_1_low']**2)**2)
+    df['gamma'], df['gamma_err'] = gamma(p.secrecy,df['v_check_1_up']/df['s_check_1_low'],
+                                        df['s_key_1_low'],df['s_check_1_low'],
+                                        err_temp, df['s_key_1_low_err'], df['s_check_1_low_err'])
+
+    #phase error finally
+    df['phi_up'] = df['v_check_1_up']/df['s_check_1_low'] + df['gamma']
+    df['phi_up_err'] = np.sqrt(err_temp**2 + df['gamma_err']**2)
+    df[df<0] = 0
+
+    def bin_entropy(x, ex):
+            entropy = -x*np.log2(x)-(1-x)*np.log2(1-x)
+            error = (np.log(1-x)-np.log(x))*ex/np.log(2)
+            return entropy, error
+
+    entropy, error = bin_entropy(df['phi_up'], df['phi_up_err'])
+
+    df['secret_key_length'] = df['s_key_0_low']+df['s_key_1_low']*(1-entropy)\
+                            - p.lambda_EC - 6*np.log2(19/p.secrecy) - np.log2(2/p.correctness)
+
+    df['secret_key_length_err'] = np.sqrt(df['s_key_0_low_err']**2 \
+                                    + (df['s_key_1_low_err']*(1-entropy))**2 \
+                                    + (df['s_key_1_low_err']*error)**2) 
+
+    df['SKR'] = df['secret_key_length']*repetition_rate/df['total_pulses']
+    df['SKR_err'] = np.sqrt( (df['secret_key_length_err']*repetition_rate/df['total_pulses'])**2 \
+                    + (df['secret_key_length']*repetition_rate*df['total_pulses_err']/df['total_pulses']**2)**2)
+
+    df.to_csv('results/{}block_{}frac_results.csv'.format(block_size, frac_data), index=False)
+
+if __name__ == "__main__":
+    args = vars(parser.parse_args())
+    main(**args)
